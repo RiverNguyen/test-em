6258c58e814e454c019fc43fd9a626021ba41127
RiverNguyen <ngiang1309@gmail.com>
2024-10-02 00:55:40 +0700

    upload 1

--- src/app/components/ListItem/page.js
+++ src/app/components/ListItem/page.js
@@ -3,26 +3,23 @@ import React from "react";
 import product1 from "@/app/images/product1.jpg";
 import Link from "next/link";
 
-export default function ListItem({ id, category, price }) {
-  // console.log("image", category.image);
-
+export default function ListItem({ id, category = {}, price }) {
   return (
-    <div className=" flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <Link href={`components/ListItem/${id}`}>
         <Image
           priority
           width={300}
           height={500}
-          src={category.image ? category.image : product1}
-          alt="Product image"
+          src={category?.image ? category.image : product1}
+          alt={category?.name || "Product image"}
           style={{ width: "100%", height: "auto" }}
-        ></Image>
+        />
       </Link>
       <div className="text-3xl font-bold">
-        {category.name ? category.name : "Product"}
+        {category?.name ? category.name : "Product"}
       </div>
-
-      <div className="text-2xl  text-red-700">Price: {price} $</div>
+      <div className="text-2xl text-red-700">Price: {price} $</div>
     </div>
   );
 }
